c9da8e8b9c6fdf2ecda66afe0b5f411a637b9ed3
SurajDad <80150336+SurajDad@users.noreply.github.com>
2021-10-25 10:54:52 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+var first_name = "Ranbir";
+var last_name = "Kapoor";
+var first_no = 10;
+var second_no = 5;
+
+var full_name = first_name.concat(last_name);
+var add = first_no + second_no
+console.log(add)
